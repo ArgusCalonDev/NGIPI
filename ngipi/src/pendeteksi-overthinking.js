fa9f60700b3c303d9bdccb7be27a9b73dcd6b5df
ArgusCalonDev <putuardhya06@gmail.com>
2026-05-04 13:50:29 +0800

    Add: pendeteksi overthinking

---
+++ ngipi/src/pendeteksi-overthinking.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('input');
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+btn.addEventListener('click', function() {
+    output.textContent = 'Tenang, itu cuma kepikiran';
+});
