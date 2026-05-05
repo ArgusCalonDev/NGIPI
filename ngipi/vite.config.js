eeabf5e1c7b6aa216c63be956e9ed118d9ed8b34
ArgusCalonDev <putuardhya06@gmail.com>
2026-05-05 14:11:10 +0800

    Edit: vite config

--- ngipi/vite.config.js
+++ ngipi/vite.config.js
@@ -13,7 +13,13 @@ export default defineConfig({
         pembuatKeputusan: resolve(__dirname, 'pembuat-keputusan.html'),
         realLogin: resolve(__dirname, 'real-login.html'),
         searchEngine: resolve(__dirname, 'search-engine.html'),
-        tombolRandom: resolve(__dirname, 'tombol-random.html')
+        tombolRandom: resolve(__dirname, 'tombol-random.html'),
+        generatorAlasanTelat: resolve(__dirname, 'generator-alasan-telat.html'),
+        tunggu60Detik: resolve(__dirname, 'tunggu-60-detik.html'),
+        pendeteksiOverthinking: resolve(__dirname, 'pendeteksi-overthinking.html'),
+        kalkulator: resolve(__dirname, 'kalkulator.html'),
+        klik1000Kali: resolve(__dirname, 'klik-1000-kali.html'),
+        janganDipencet: resolve(__dirname, 'jangan-dipencet.html')
       },
     },
   },
