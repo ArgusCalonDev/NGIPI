a3988ca17299cd224219c0b3746feea6e34d922b
ArgusCalonDev <putuardhya06@gmail.com>
2026-05-28 10:13:49 +0800

    Add: output selanjutnya di tombol random

--- ngipi/src/tombol-random.js
+++ ngipi/src/tombol-random.js
@@ -1,8 +1,19 @@
 const btn = document.getElementById('btn-random');
 const output = document.getElementById('output');
 
+let count = 0;
+
 output.style.display = 'none';
 
 btn.addEventListener('click', function() {
+    count++;
+    if (count === 10) {
+        output.textContent = 'WAH BENER BENER GABUT NIH ORANG';
+    } else if (count === 20) {
+        output.textContent = 'Perihal apa yang membuatmu melakukan ini?';
+    } else if (count === 50) {
+        output.textContent = 'TAMAT';
+        return;
+    }
     output.style.display = 'block';
 });
