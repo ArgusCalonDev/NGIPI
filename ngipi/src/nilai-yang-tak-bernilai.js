229e0387f322c38f68ca8836f00b55bb3982b77e
ArgusCalonDev <putuardhya06@gmail.com>
2026-06-22 10:07:59 +0800

    Edit: waktu perubahan nilai menjadi 10 detik

--- ngipi/src/nilai-yang-tak-bernilai.js
+++ ngipi/src/nilai-yang-tak-bernilai.js
@@ -3,7 +3,7 @@ const nilai = document.getElementById('nilai');
 function generatePersentase() {
     const persentase = Math.floor(Math.random() * 101);
     nilai.textContent = `${persentase} %`;
-    setTimeout(generatePersentase, 60000);
+    setTimeout(generatePersentase, 10000);
 }
 
 generatePersentase();
